0a824db6ed2045f7671f0fca6625d9dba8b7ec30
Miguel Rebollo <mrebollo@upv.es>
2025-09-24 05:47:17 +0200

    variación sin instrucción de selección (if)

--- tema4/for/ejer7.c
+++ tema4/for/ejer7.c
@@ -1,12 +1,5 @@
 /*
 Leer n y contar cuántos múltiplos de 3 hay entre 1 y n.
-
-Pista: el resto de la división entera se obtiene
-con el operador % (módulo)
-Ejemplo: 10 % 3 = 1, 9 % 3 = 0, 8 % 3 = 2
-
-Los divisores entre 3 son aquellos números
-que al dividirlos entre 3 dan resto 0.
 */
 
 #include <stdio.h>
@@ -16,10 +9,12 @@ int main() {
     printf("Escribe un número: ");
     scanf("%d", &n);
     int contador = 0; // inicializamos el contador a 0
-    for (int i = 1; i <= n; i++) {
-        if (i % 3 == 0) { // si i es múltiplo de 3
-            contador++;   // incrementamos el contador
-        }
+    // recorremos los números desde 3 hasta n de 3 en 3
+    // (solo pasamos por los múltiplos de 3)
+    // si n < 3, no entra en el bucle -> contador == 0
+    for (int i = 3; i <= n; i += 3) {
+        // incrementamos los múltiplos de 3
+        contador++;
     }
     printf("Hay %d múltiplos de 3 entre 1 y %d\n", contador, n);
     return 0;
